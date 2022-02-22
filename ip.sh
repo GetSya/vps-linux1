b7c4aa6af625fba711e2506ee9e378d1d5b1fb74
ItzMepHazn0010 <94892065+ItzMepHazn0010@users.noreply.github.com>
2022-02-22 17:19:05 +0700

    Update!
    Nothing

--- ip.sh
+++ ip.sh
@@ -1,7 +1,7 @@
 _login=$(grep "command failed" < .ngrok.log)
 
 if [[ -z "$_login" ]]; then
-    echo $(grep -o -E "tcp://(.+)" < .ngrok.log")
+    echo $(grep -o -E "tcp://(.+)" < .ngrok.log)
 else
     echo "$_login"
     exit 4
